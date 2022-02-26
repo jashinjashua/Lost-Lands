b58123029ec40747939ffde78f111a20d73e7c0e
Jashin Jashua <jashin.jashua@gmail.com>
2022-02-26 15:49:08 +0400

    starts from level0.json

--- Lost_Lands/js/main.js
+++ Lost_Lands/js/main.js
@@ -497,7 +497,7 @@ window.onload = function () {
   let game = new Phaser.Game(960, 600, Phaser.AUTO, "game");
 
   game.state.add("play", PlayState);
-  game.state.start("play", true, false, { level: 0 });
+  game.state.start("play", true, false, { level: 7 });
 };
 
 PlayState.preload = function () {
